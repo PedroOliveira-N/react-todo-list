59754be2059e269ec2b3d9b365edb146f3125a34
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2026-01-21 15:16:23 -0300

    adicionando capacidade de adicionar tarefa

--- src/components/ToDoForm/index.jsx
+++ src/components/ToDoForm/index.jsx
@@ -2,11 +2,15 @@ import Button from '../Button';
 import TextInput from '../TextInput';
 import './todo-form.style.css'
 
-export default function ToDoForm(onSubmit) {
+export default function ToDoForm({ onSubmit }) {
   return (
     <div>
       <form action={onSubmit} className='todo-form'>
-        <TextInput placeholder="Digite o item que deseja adicionar" />
+        <TextInput 
+          placeholder="Digite o item que deseja adicionar"
+          required
+          name="description"
+        />
 
         <Button>Salvar item</Button>
       </form>
